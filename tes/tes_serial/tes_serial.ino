1aa208ee8edc2187bf77f17bc8a072c3c8ea04ac
Taufik Hidayat <taufikdev88@gmail.com>
2020-06-08 20:50:01 +0700

    update random

--- tes/tes_serial/tes_serial.ino
+++ tes/tes_serial/tes_serial.ino
@@ -13,13 +13,13 @@ void loop()
   doc["Time"] = 123132123;
   doc["Latitude"] = -1213.231232;
   doc["Longitude"] = 7.454545;
-  doc["Current"] = 5.1;
-  doc["Voltage"] = 12.0;
-  doc["WaveHeight"] = 12.12;
-  doc["WavePeriod"] = 34.23;
-  doc["WavePower"] = 43.23;
-  doc["AirTemp"] = 23.43;
-  doc["WaterTemp"] = 231.23;
+  doc["Current"] = random(1,100) / 10.0;
+  doc["Voltage"] = random(1,100) / 10.0;
+  doc["WaveHeight"] = random(1,100) / 10.0;
+  doc["WavePeriod"] = random(1,100) / 10.0;
+  doc["WavePower"] = random(1,100) / 10.0;
+  doc["AirTemp"] = random(1,100) / 10.0;
+  doc["WaterTemp"] = random(1,100) / 10.0;
   
   serializeJson(doc, Serial);
   Serial.println();
